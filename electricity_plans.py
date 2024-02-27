b8ff1f0dca14d10ef5a4bd0a3fbc792b4c3cf655
David Hooton <david@hooton.org>
2024-02-27 15:48:17 +1100

    Skipped unnecessary rows when loading provider URLs.

--- electricity_plans.py
+++ electricity_plans.py
@@ -6,8 +6,7 @@ from datetime import datetime
 def load_provider_urls(filename):
     with open(filename, newline='') as csvfile:
         reader = csv.DictReader(csvfile)
-        next(csvfile)  # Skip the first row (title row)
-        next(csvfile)  # Skip the second row (header row)
+        next(reader, None)  # Skip the header row
         return {row['Brand Name']: row['Retailer Base URI'] for row in reader if row['Brand Name']}
 
 def fetch_plans(base_url, headers):
