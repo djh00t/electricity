590ce0c2de5c55fa00543d71f3b5501721612a2e
David Hooton <david@hooton.org>
2024-03-11 14:34:22 +1100

    Add config file
    Signed-off-by: David Hooton <david@hooton.org>

--- config.py
+++ config.py
@@ -3,8 +3,6 @@ Configuration settings for the project.
 """
 RETAILER_PDF_URL = 'https://www.aer.gov.au/documents/consumer-data-right-list-energy-retailer-base-uris-june-2023'
 
-# How often plans.json should be updated (in seconds)
-
 # Number of days after which the plan should be refreshed
 REFRESH_DAYS = 7
 
